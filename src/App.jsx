be6885ce09abcbd057ba6db1ff211739f1aa8a7e
Cezary Burzykowski <cezaryburzykowski@Cezarys-MacBook-Pro.local>
2019-09-14 15:19:42 +0200

    login screen prepared

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AppState from './context';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import SignInScreen from './screens/SignInScreen';
 import MapScreen from './screens/MapScreen';
 
+
 function App() {
+  const [ticketNumber, setTicketNumber] = useState('');
+
   return (
-    <Router>
-      <Route exact path='/' component={SignInScreen} />
-      <Route path='/journey' component={MapScreen} />
-    </Router>
+    <AppState.Provider value={{
+      ticketNumber,
+      setTicketNumber
+    }}>
+      <Router>
+        <Route exact path='/' component={SignInScreen} />
+        <Route path='/journey' component={MapScreen} />
+      </Router>
+    </AppState.Provider>
   );
 }
 
--- src/screens/SignInScreen/index.jsx
+++ src/screens/SignInScreen/index.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
-import SignInContainer from '../../components/SignInContainer';
+import React, { useContext, useCallback } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import AppState from '../../context';
 import Navigation from '../../components/Navigation';
-import Logo from '../../components/Logo';
+import './index.scss';
 
-const SignInScreen = () => {
+const SignInScreen = ({ history: { push } }) => {
+  const { ticketNumber, setTicketNumber } = useContext(AppState);
+  const handleSubmit = useCallback(() => {
+    if (ticketNumber) {
+      push('/journey');
+    }
+  }, [ticketNumber]);
+
   return (
-    <SignInContainer>
-      <Logo />
+    <div className='sign-in'>
       <Navigation />
-    </SignInContainer>
+      <form className='sign-in__form'>
+        <TextField
+          id="outline-required"
+          label="Enter ticket number"
+          value={ticketNumber}
+          onChange={(e) => setTicketNumber(e.target.value)}
+          margin="normal"
+          variant="filled"
+          style={{ width: '100%' }}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ backgroundColor: '#263761', width: '100%' }}
+          onClick={handleSubmit}
+        >
+          Start your journey
+        </Button>
+      </form>
+    </div>
   );
 }
 
