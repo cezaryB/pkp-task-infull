e1f25d5f46bd95ef860e412706d78a4e4360d824
Mateusz Sosnowski <mateusz.sosnowski@infullmobile.com>
2019-09-14 14:00:40 +0200

    Change zip component to material-ui

--- src/components/Zip/index.jsx
+++ src/components/Zip/index.jsx
@@ -1,25 +1,18 @@
 import React from "react";
+import Slider from "@material-ui/core/Slider";
 import "./index.scss";
 
-const Zip = (props) => {
+const Zip = props => {
   const [zipValue, setZipValue] = React.useState(0);
 
-  const handleChange = () => e => {
-    const value = 
-    setZipValue(e.target.value);
-    props.handleCurrentPosition(e.target.value)
+  const handleChange = (_, value) => {
+    setZipValue(value);
+    props.handleCurrentPosition(value);
   };
 
   return (
     <div className="zip_container">
-      <input
-        type="range"
-        name="point-position"
-        min="0"
-        max="20"
-        value={zipValue}
-        onChange={handleChange()}
-      />
+      <Slider onChange={handleChange} value={zipValue} min={0} max={20} />
       <label className="zip_label">Move train</label>
     </div>
   );
