42e345826afe8d1aad3ad4e4b4fc032ece4c475c
Cezary Burzykowski <cezaryburzykowski@Cezarys-MacBook-Pro.local>
2019-09-14 11:30:36 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomeScreen from './screens/Home';
+
+function App() {
+  return (
+    <Router>
+      <Route path='/' component={HomeScreen} />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/screens/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExampleComponent from '../../components/ExampleComponent';
+
+const HomeScreen = (props) => {
+  return (
+    <div>
+      <ExampleComponent />
+    </div>
+  )
+}
+
+export default HomeScreen;
